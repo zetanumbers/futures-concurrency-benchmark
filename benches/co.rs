55c34b2b6b621cb767e048580e4d0c91d0052743
Daria Sukhonina <dariasukhonina@gmail.com>
2024-06-09 00:33:20 +0300

    add yield_now variations

--- benches/co.rs
+++ benches/co.rs
@@ -1,109 +1,126 @@
-use std::future::{ready, Ready};
+use std::{future::Future, iter, pin::pin};
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{
+    black_box, criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup, Criterion,
+};
 use futures_concurrency::future::{FutureGroup, Join};
-use futures_lite::{future::block_on, StreamExt};
+use futures_lite::{
+    future::{block_on, yield_now},
+    StreamExt,
+};
 
 const JOIN_SIZE: usize = 1_000_000;
 const SAMPLE_SIZE: usize = 10;
-fn async_work<T>(x: T) -> Ready<T> {
-    ready(x)
-}
 
-fn seq(c: &mut Criterion) {
-    c.benchmark_group("seq")
-        .sample_size(SAMPLE_SIZE)
-        .bench_function("seq", |b| {
-            b.iter(|| {
-                block_on(async {
-                    for _ in 0..JOIN_SIZE {
-                        black_box(async_work(1).await);
-                    }
-                });
-            });
-        });
-}
-
-fn join(c: &mut Criterion) {
-    c.benchmark_group("join")
-        .sample_size(SAMPLE_SIZE)
-        .bench_function("futures_concurrency::join", |b| {
-            b.iter(|| {
-                block_on(async {
-                    let mut futures = Vec::with_capacity(JOIN_SIZE);
-                    futures.resize_with(JOIN_SIZE, || async_work(1));
-                    black_box(Join::join(futures).await);
-                });
-            });
-        });
-}
-
-fn group(c: &mut Criterion) {
-    c.benchmark_group("group")
-        .sample_size(SAMPLE_SIZE)
-        .bench_function("futures_concurrency::FutureGroup", |b| {
-            let mut group = FutureGroup::with_capacity(JOIN_SIZE);
-            b.iter(|| {
-                block_on(async {
-                    for _ in 0..JOIN_SIZE {
-                        group.insert(async_work(1));
-                    }
-
-                    while let Some(x) = group.next().await {
-                        black_box(x);
-                    }
-                    black_box(&mut group);
-                });
-            });
-        });
-}
-
-fn spawn(c: &mut Criterion) {
-    c.benchmark_group("spawn_local")
-        .sample_size(SAMPLE_SIZE)
-        .bench_function("async_executor::LocalExecutor", |b| {
-            let ex = async_executor::LocalExecutor::new();
-            let mut tasks = Vec::with_capacity(JOIN_SIZE);
-            b.iter(|| {
-                block_on(ex.run(async {
-                    tasks.resize_with(JOIN_SIZE, || ex.spawn(async_work(1)));
-
-                    for task in tasks.drain(..) {
-                        black_box(task.await);
-                    }
-                }));
-            })
-        })
-        .bench_function("unsend::executor::Executor", |b| {
-            let ex = unsend::executor::Executor::new();
-            let mut tasks = Vec::with_capacity(JOIN_SIZE);
-            b.iter(|| {
-                block_on(ex.run(async {
-                    tasks.resize_with(JOIN_SIZE, || ex.spawn(async_work(1)));
-
-                    for task in tasks.drain(..) {
-                        black_box(task.await);
-                    }
-                }));
-            })
-        });
-    c.benchmark_group("spawn")
-        .sample_size(SAMPLE_SIZE)
-        .bench_function("async_executor::Executor", |b| {
-            let ex = async_executor::Executor::new();
-            let mut tasks = Vec::with_capacity(JOIN_SIZE);
-            b.iter(|| {
-                block_on(ex.run(async {
-                    tasks.resize_with(JOIN_SIZE, || ex.spawn(async_work(1)));
-
-                    for task in tasks.drain(..) {
-                        black_box(task.await);
-                    }
-                }));
-            })
-        });
-}
-
-criterion_group!(benches, seq, join, group, spawn);
+criterion_group!(benches, all);
 
 criterion_main!(benches);
+
+fn all(c: &mut Criterion) {
+    shallow_many(
+        c.benchmark_group("ready_work").sample_size(SAMPLE_SIZE),
+        ready_work,
+    );
+    shallow_many(
+        c.benchmark_group("yield_now_work").sample_size(SAMPLE_SIZE),
+        yield_now_work,
+    );
+}
+
+async fn ready_work() -> i32 {
+    1
+}
+
+async fn yield_now_work() -> i32 {
+    yield_now().await;
+    1
+}
+
+fn shallow_many_local<F, T, M>(c: &mut BenchmarkGroup<'_, M>, work: F)
+where
+    F: Fn() -> T + Copy + 'static,
+    T: Future,
+    M: Measurement,
+{
+    c.bench_function("seq", |b| {
+        b.iter(|| {
+            block_on(async {
+                for _ in 0..JOIN_SIZE {
+                    black_box(work().await);
+                }
+            });
+        });
+    })
+    .bench_function("futures_concurrency::join", |b| {
+        b.iter(|| {
+            block_on(async {
+                let mut futures = Vec::with_capacity(JOIN_SIZE);
+                futures.resize_with(JOIN_SIZE, work);
+                black_box(Join::join(futures).await);
+            });
+        });
+    })
+    .bench_function("futures_concurrency::FutureGroup", |b| {
+        b.iter(|| {
+            block_on(async {
+                let mut group = pin!(iter::repeat_with(work)
+                    .take(JOIN_SIZE)
+                    .collect::<FutureGroup<_>>());
+
+                while let Some(x) = group.next().await {
+                    black_box(x);
+                }
+                black_box(&mut group);
+            });
+        });
+    })
+    .bench_function("async_executor::LocalExecutor", |b| {
+        let ex = async_executor::LocalExecutor::new();
+        let mut tasks = Vec::with_capacity(JOIN_SIZE);
+        b.iter(|| {
+            block_on(ex.run(async {
+                tasks.resize_with(JOIN_SIZE, || ex.spawn(work()));
+
+                for task in tasks.drain(..) {
+                    black_box(task.await);
+                }
+            }));
+        })
+    })
+    .bench_function("unsend::executor::Executor", |b| {
+        let ex = unsend::executor::Executor::new();
+        let mut tasks = Vec::with_capacity(JOIN_SIZE);
+        b.iter(|| {
+            block_on(ex.run(async {
+                tasks.resize_with(JOIN_SIZE, || ex.spawn(work()));
+
+                for task in tasks.drain(..) {
+                    black_box(task.await);
+                }
+            }));
+        })
+    });
+}
+
+fn shallow_many<F, T, M>(c: &mut BenchmarkGroup<'_, M>, work: F)
+where
+    F: Fn() -> T + Copy + Send + Sync + 'static,
+    T: Future + Send + Sync,
+    T::Output: Send,
+    M: Measurement,
+{
+    shallow_many_local(c, work);
+    c.bench_function("async_executor::Executor", |b| {
+        let ex = async_executor::Executor::new();
+        let mut tasks = Vec::with_capacity(JOIN_SIZE);
+        b.iter(|| {
+            block_on(ex.run(async {
+                tasks.resize_with(JOIN_SIZE, || ex.spawn(work()));
+
+                for task in tasks.drain(..) {
+                    black_box(task.await);
+                }
+            }));
+        })
+    });
+}

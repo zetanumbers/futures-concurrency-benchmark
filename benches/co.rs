c5781661a1195a4e4431a5eb80f38cdf80713562
Daria Sukhonina <dariasukhonina@gmail.com>
2024-06-09 17:07:13 +0300

    small improvements

--- benches/co.rs
+++ benches/co.rs
@@ -46,7 +46,11 @@ struct TokioParams {
 
 impl fmt::Display for TokioParams {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{self:?}")
+        self.tasks.fmt(f)?;
+        if let TokioRuntimeFlavor::MultiThread = self.rt {
+            write!(f, "/{}", num_cpus::get())?
+        }
+        Ok(())
     }
 }
 
@@ -65,32 +69,27 @@ impl TokioRuntimeFlavor {
     }
 }
 
-impl fmt::Display for TokioRuntimeFlavor {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            TokioRuntimeFlavor::CurrentThread => "current_thread",
-            TokioRuntimeFlavor::MultiThread => "multi_thread",
-        }
-        .fmt(f)
-    }
-}
-
 fn shallow_many_local<F, T, M>(c: &mut BenchmarkGroup<'_, M>, work: F, task_count: usize)
 where
     F: Fn() -> T + Copy + 'static,
     T: Future + 'static,
     M: Measurement,
 {
-    c.bench_function(BenchmarkId::new("seq", task_count), |b| {
-        b.to_async(FuturesExecutor).iter(|| async {
-            for _ in 0..task_count {
-                black_box(work().await);
-            }
-        });
-    })
-    .bench_function(
+    c.bench_with_input(
+        BenchmarkId::new("seq", task_count),
+        &task_count,
+        |b, &task_count| {
+            b.to_async(FuturesExecutor).iter(|| async {
+                for _ in 0..task_count {
+                    black_box(work().await);
+                }
+            });
+        },
+    )
+    .bench_with_input(
         BenchmarkId::new("futures_concurrency::join", task_count),
-        |b| {
+        &task_count,
+        |b, &task_count| {
             b.to_async(FuturesExecutor).iter(|| async {
                 let mut futures = Vec::with_capacity(task_count);
                 futures.resize_with(task_count, work);
@@ -98,9 +97,10 @@ where
             });
         },
     )
-    .bench_function(
+    .bench_with_input(
         BenchmarkId::new("futures_concurrency::FutureGroup", task_count),
-        |b| {
+        &task_count,
+        |b, &task_count| {
             b.to_async(FuturesExecutor).iter(|| async {
                 let mut group = pin!(iter::repeat_with(work)
                     .take(task_count)
@@ -113,9 +113,10 @@ where
             });
         },
     )
-    .bench_function(
+    .bench_with_input(
         BenchmarkId::new("async_executor::LocalExecutor", task_count),
-        |b| {
+        &task_count,
+        |b, &task_count| {
             let ex = async_executor::LocalExecutor::new();
             b.to_async(FuturesExecutor).iter(|| {
                 ex.run(async {
@@ -129,9 +130,10 @@ where
             })
         },
     )
-    .bench_function(
+    .bench_with_input(
         BenchmarkId::new("unsend::executor::Executor", task_count),
-        |b| {
+        &task_count,
+        |b, &task_count| {
             let ex = unsend::executor::Executor::new();
             b.to_async(FuturesExecutor).iter(|| {
                 ex.run(async {
@@ -153,25 +155,35 @@ where
             rt,
             tasks: task_count,
         };
-        c.bench_function(BenchmarkId::new("tokio::task::spawn_local", param), |b| {
-            let rt = param.rt.builder().build().unwrap();
-            b.to_async(rt).iter(|| async {
-                let mut tasks = Vec::with_capacity(task_count);
-                tasks.resize_with(task_count, || tokio::task::spawn_local(work()));
+        c.bench_with_input(
+            BenchmarkId::new("tokio::task::LocalSet::spawn_local", param),
+            &param,
+            |b, &param| {
+                let rt = param.rt.builder().build().unwrap();
+                b.to_async(rt).iter(|| async {
+                    let local_set = LocalSet::new();
+                    let mut tasks = Vec::with_capacity(param.tasks);
+                    tasks.resize_with(param.tasks, || local_set.spawn_local(work()));
 
-                for task in tasks.drain(..) {
-                    black_box(task.await.unwrap());
-                }
-            })
-        })
-        .bench_function(
-            BenchmarkId::new("tokio::task::JoinSet::spawn_local", param),
-            |b| {
+                    local_set
+                        .run_until(async {
+                            for task in tasks.drain(..) {
+                                black_box(task.await.unwrap());
+                            }
+                        })
+                        .await;
+                })
+            },
+        )
+        .bench_with_input(
+            BenchmarkId::new("tokio::task::JoinSet::spawn_local_on", param),
+            &param,
+            |b, &param| {
                 let rt = param.rt.builder().build().unwrap();
                 b.to_async(rt).iter(|| async {
                     let local_set = LocalSet::new();
                     let mut set = JoinSet::new();
-                    for _ in 0..task_count {
+                    for _ in 0..param.tasks {
                         set.spawn_local_on(work(), &local_set);
                     }
 
@@ -197,9 +209,10 @@ where
     M: Measurement,
 {
     shallow_many_local(c, work, task_count);
-    c.bench_function(
+    c.bench_with_input(
         BenchmarkId::new("async_executor::Executor", task_count),
-        |b| {
+        &task_count,
+        |b, &task_count| {
             let ex = async_executor::Executor::new();
             b.to_async(FuturesExecutor).iter(|| {
                 ex.run(async {
@@ -221,24 +234,29 @@ where
             rt,
             tasks: task_count,
         };
-        c.bench_function(BenchmarkId::new("tokio::task::spawn", param), |b| {
-            let rt = param.rt.builder().build().unwrap();
-            b.to_async(rt).iter(|| async {
-                let mut tasks = Vec::with_capacity(task_count);
-                tasks.resize_with(task_count, || tokio::task::spawn(work()));
+        c.bench_with_input(
+            BenchmarkId::new("tokio::task::spawn", param),
+            &param,
+            |b, &param| {
+                let rt = param.rt.builder().build().unwrap();
+                b.to_async(rt).iter(|| async {
+                    let mut tasks = Vec::with_capacity(param.tasks);
+                    tasks.resize_with(param.tasks, || tokio::task::spawn(work()));
 
-                for task in tasks.drain(..) {
-                    black_box(task.await.unwrap());
-                }
-            })
-        })
-        .bench_function(
+                    for task in tasks.drain(..) {
+                        black_box(task.await.unwrap());
+                    }
+                })
+            },
+        )
+        .bench_with_input(
             BenchmarkId::new("tokio::task::JoinSet::spawn", param),
-            |b| {
+            &param,
+            |b, &param| {
                 let rt = param.rt.builder().build().unwrap();
                 b.to_async(rt).iter(|| async {
                     let mut set = iter::repeat_with(work)
-                        .take(task_count)
+                        .take(param.tasks)
                         .collect::<JoinSet<_>>();
 
                     while let Some(x) = set.join_next().await {
